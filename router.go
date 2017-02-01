d9a194181ac3aa1a12a5f53f51137c821ba29788
cssivision <cssivision@gmail.com>
2017-02-01 18:22:36 +0800

    feat: add code comment

--- router.go
+++ router.go
@@ -5,18 +5,37 @@ import (
 	"strings"
 )
 
+// Router is a http.Handler which can be used to dispatch requests to different
+// handler functions via configurable routes
 type Router struct {
-	tree                  *node
-	IgnoreCase            bool
+	tree *node
+
+	// Ignore case when matching URL path.
+	IgnoreCase bool
+
+	// Enables automatic redirection if the current route can't be matched but a
+	// handler for the path with (without) the trailing slash exists.
+	// TrailingSlashRedirect: /a/b/ -> /a/b
+	// TrailingSlashRedirect: /a/b -> /a/b/
 	TrailingSlashRedirect bool
-	NotFound              http.Handler
-	BasePath              string
+
+	// Configurable http.Handler which is called when no matching route is
+	// found. If it is not set, http.NotFound is used.
+	NotFound http.Handler
+
+	// Prefix path of a router
+	BasePath string
 }
 
+// Handle is a function that can be registered to a route to handle HTTP
+// requests. Like http.HandlerFunc, but has a third parameter for the
+// values of named/wildcards parameters.
 type Handle func(http.ResponseWriter, *http.Request, Params)
 
+// Param is a single URL parameter, a map[string]string.
 type Params map[string]string
 
+// New returns a new initialized Router, with default configuration
 func New() *Router {
 	return &Router{
 		tree: &node{
@@ -28,38 +47,47 @@ func New() *Router {
 	}
 }
 
+// Get is a shortcut for router.Handle("GET", path, handle)
 func (r *Router) Get(pattern string, handler Handle) {
 	r.Handle(http.MethodGet, pattern, handler)
 }
 
+// Post is a shortcut for router.Handle("POST", path, handle)
 func (r *Router) Post(pattern string, handler Handle) {
 	r.Handle(http.MethodPost, pattern, handler)
 }
 
+// Put is a shortcut for router.Handle("PUT", path, handle)
 func (r *Router) Put(pattern string, handler Handle) {
 	r.Handle(http.MethodPut, pattern, handler)
 }
 
+// Delete is a shortcut for router.Handle("DELETE", path, handle)
 func (r *Router) Delete(pattern string, handler Handle) {
 	r.Handle(http.MethodDelete, pattern, handler)
 }
 
+// Options is a shortcut for router.Handle("OPTIONS", path, handle)
 func (r *Router) Options(pattern string, handler Handle) {
 	r.Handle(http.MethodOptions, pattern, handler)
 }
 
+// Trace is a shortcut for router.Handle("TRACE", path, handle)
 func (r *Router) Trace(pattern string, handler Handle) {
 	r.Handle(http.MethodTrace, pattern, handler)
 }
 
+// Head is a shortcut for router.Handle("HEAD", path, handle)
 func (r *Router) Head(pattern string, handler Handle) {
 	r.Handle(http.MethodHead, pattern, handler)
 }
 
+// Patch is a shortcut for router.Handle("PATCH", path, handle)
 func (r *Router) Patch(pattern string, handler Handle) {
 	r.Handle(http.MethodPatch, pattern, handler)
 }
 
+// Add prefix for a router, and return a new one
 func (r *Router) Prefix(prefix string) *Router {
 	if prefix == "" {
 		panic("prefix can not be empty")
@@ -82,6 +110,9 @@ func (r *Router) Prefix(prefix string) *Router {
 	}
 }
 
+// Handle registers a new request handle with the given path and method.
+// For GET, POST, PUT, PATCH and DELETE requests the respective shortcut
+// functions can be used.
 func (r *Router) Handle(method, pattern string, handler Handle) {
 	if method == "" {
 		panic("invalid http method")
@@ -108,6 +139,7 @@ func (r *Router) Handle(method, pattern string, handler Handle) {
 	r.tree.insert(pattern).addHandle(method, handler)
 }
 
+// ServeHTTP makes the router implement the http.Handler interface.
 func (r *Router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	var pattern string
 	if r.IgnoreCase {
